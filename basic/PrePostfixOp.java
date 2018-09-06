8d4b5abea551b68ab1d8d6b7b2904444e66bbe8e
Haymee <hmpark0502@gmail.com>
2018-09-06 22:11:00 +0900

    JAVA 수업 2일째

--- basic/PrePostfixOp.java
+++ basic/PrePostfixOp.java
@@ -5,16 +5,16 @@ class PrePostfixOp
 		int n1 = 7;
 		int n2, n3;
 
-		n2 = ++n1;
-		n3 = --n1;
+		n2 = ++n1;	// n1의 값을 1 증가 후 n2에 대입
+		n3 = --n1;		// n1의 값을 1 감소 후 n3에 대입
 
 		System.out.println(n1);	//	7
 		System.out.println(n2);	// 8
 		System.out.println(n3);	// 7
 
-		n1 = 7;
-		n2 = n1++;
-		n3 = n1--;
+		n1 = 7;			
+		n2 = n1++;	// n1의 값을 n2에 대입 후 1 증가
+		n3 = n1--;		// n1의 값을 n3에 대입 후 1 감소
 
 		System.out.println(n1);	//	7
 		System.out.println(n2);	// 7
