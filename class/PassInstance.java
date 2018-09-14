fc9c2ec2e25decd642333fcd22ea2a1a3a9da7fa
Haymee <hmpark0502@gmail.com>
2018-09-14 20:01:53 +0900

    Add project

---
+++ class/PassInstance.java
@@ -0,0 +1,30 @@
+class NumberEx
+{
+	int num = 0;
+
+	public void addNum(int n) {
+		num += n;
+	}
+
+	public int getNumber() {
+		return num;
+	}
+}
+
+class  PassInstance
+{
+	public static void main(String[] args) 
+	{
+		NumberEx ne = new NumberEx();
+
+		System.out.println("Before Call:" + ne.getNumber());
+
+		simpleMethod(ne);
+		System.out.println("After Call:" + ne.getNumber());
+
+	}
+
+	public static void simpleMethod(NumberEx numb) {
+		numb.addNum(12);
+	}
+}
