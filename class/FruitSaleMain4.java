13702fb46a06a742a613628651da16f1264189a3
Haymee <hmpark0502@gmail.com>
2018-09-14 22:15:43 +0900

    5번째 수업

---
+++ class/FruitSaleMain4.java
@@ -0,0 +1,83 @@
+class FruitSeller2 {
+    int numOfApple;
+    int myMoney;
+    final int APPLE_PRICE;
+    
+    public FruitSeller2(int money, int appleNum, int price) {
+	System.out.println("init price: " + price);
+	myMoney = money;
+	numOfApple = appleNum;
+	APPLE_PRICE = price;
+    }
+    
+    /**
+     * 사과판매 메소드, 돈을 받고 사과개수 리턴
+     * @param money
+     * @return
+     */
+    public int saleApple(int money) {
+	
+	System.out.println("사과 가격: " + APPLE_PRICE);
+	
+	// 구매액에 따른 판매할 사과개수
+	int num = money / APPLE_PRICE;
+	
+	// 보유하고 있는 사과에서 판매된 사과개수 뺌
+	numOfApple -= num;	
+	
+	// 구매금액을 매출액에 추가
+	myMoney += money;
+	
+	// 판매한 사과개수 리턴
+	return num;
+    }
+    
+    public void showSaleResult() {
+	System.out.println("남은 사과: " + numOfApple);
+	System.out.println("판매 수익: " + myMoney);
+    }
+}
+
+class FruitBuyer2 {
+    int myMoney;	// 사과 구매 가능한 금액
+    int numOfApple;	// 구매한 사과 개수
+    
+    public FruitBuyer2(int money) {
+	myMoney = money;
+	numOfApple = 0;
+    }
+    
+    public void buyApple(FruitSeller2 seller, int money) {
+	
+	// 사과 구매
+	numOfApple += seller.saleApple(money);
+	
+	myMoney -= seller.APPLE_PRICE * numOfApple;
+    }
+    
+    public void showBuyResult() {
+	System.out.println("보유 사과: " + numOfApple);
+	System.out.println("현재 잔액: " + myMoney);
+    }
+}
+
+class FruitSaleMain4 {
+    public static void main(String[] args) {
+	FruitSeller2 seller1 = new FruitSeller2(0, 30, 1500);
+	FruitBuyer2 buyer = new FruitBuyer2(10000);
+
+	seller1.myMoney += 500;
+	buyer.myMoney -= 500;
+
+	seller1.numOfApple -= 20;
+	buyer.numOfApple += 20;
+	
+	
+	System.out.println("판매자1 현재 상황");
+	seller1.showSaleResult();
+	
+	
+	System.out.println("구매자 현재 상황");
+	buyer.showBuyResult();
+    }
+}
--- class/PassInstance.java
+++ class/PassInstance.java
@@ -1,30 +1,27 @@
-class NumberEx
-{
-	int num = 0;
+class NumberEx {
+    int num = 0; // 멤버변수(인스턴스변수)
 
-	public void addNum(int n) {
-		num += n;
-	}
+    public void addNum(int n) {
+	num += n;
+    }
 
-	public int getNumber() {
-		return num;
-	}
+    public int getNumber() {
+	return num;
+    }
 }
 
-class  PassInstance
-{
-	public static void main(String[] args) 
-	{
-		NumberEx ne = new NumberEx();
+class PassInstance {
+    public static void main(String[] args) {
+	NumberEx ne = new NumberEx();
 
-		System.out.println("Before Call:" + ne.getNumber());
+	System.out.println("Before Call:" + ne.getNumber());
 
-		simpleMethod(ne);
-		System.out.println("After Call:" + ne.getNumber());
+	simpleMethod(ne);
+	System.out.println("After Call:" + ne.getNumber());
 
-	}
+    }
 
-	public static void simpleMethod(NumberEx numb) {
-		numb.addNum(12);
-	}
+    public static void simpleMethod(NumberEx numb) {
+	numb.addNum(12);
+    }
 }
